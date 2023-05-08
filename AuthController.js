9ffd08f2e7a9746f820a137d981c8986c03c2bd6
Manish Chitre <manish_chitre@hotmail.com>
2023-05-08 15:35:26 +0530

    added line 4

--- AuthController.js
+++ AuthController.js
@@ -1,6 +1,7 @@
 function authenticate(token){
  const authToken = '#'+ Date.now() + token;
  console.log('This is authentication' + authToken);
+ console.log('Well manish here on the working.. tree');
 }
 
 function verify(){
