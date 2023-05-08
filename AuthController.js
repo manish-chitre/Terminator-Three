5f8432d24251fc589fc9774c7390cf9613766a5f
Manish Chitre <manish_chitre@hotmail.com>
2023-05-08 15:38:14 +0530

    added line 4

--- AuthController.js
+++ AuthController.js
@@ -1,6 +1,7 @@
 function authenticate(token){
  const authToken = '#'+ Date.now() + token;
  console.log('This is authentication' + authToken);
+ console.log('This is something we need - manish - 2tyuei!')
 }
 
 function verify(){
